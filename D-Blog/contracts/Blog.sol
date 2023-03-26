941a54ec5713a6fc8fbffe2699907de40b537b19
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-26 23:09:35 +0530

    Third-Contract Origin

--- D-Blog/contracts/Blog.sol
+++ D-Blog/contracts/Blog.sol
@@ -9,12 +9,18 @@ contract Blog {
         string description;
         uint256 id;
     }
+    address public owner;
+    event sendItem(address creator, address receiver, uint256 id);
     event createItem(
         string title,
         address indexed creator,
         string description,
         uint256 id
     );
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     mapping(uint256 => Post) public posts;
     uint256 public totalPost;
@@ -27,9 +33,18 @@ contract Blog {
         emit createItem(_title, msg.sender, _description, totalPost);
     }
 
-    function updatePost(string memory _name, string memory _description,uint256 _id) external 
-    {
-        
+    function sendPost(address _receiver, uint256 _id) external {
+        emit sendItem(msg.sender, _receiver, _id);
+        owner = _receiver;
+    }
+
+    function updatePost(
+        string memory _title,
+        string memory _description,
+        uint256 _id
+    ) external {
+        posts[_id].title = _title;
+        posts[_id].description = _description;
     }
 
     function deletePost(uint256 _id) external {
